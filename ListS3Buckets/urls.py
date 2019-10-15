2dbf6031c7c53b4757c00c87c4c753ea39ee108d
Ali <ali@binate-solutions.com>
2019-10-15 15:04:51 +0600

    Added pytest framework. Adding basic test

--- ListS3Buckets/urls.py
+++ ListS3Buckets/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('listS3Documents.urls')),
+    path('', include('listS3Documents.urls', namespace='list_s3_documents')),
     path('admin/', admin.site.urls),
 ]
--- listS3Documents/views.py
+++ listS3Documents/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import boto3
+
 # Let's use Amazon S3
 s3 = boto3.client('s3')
 session = boto3.Session()
+
+
 def index(request):
     documents = s3.list_buckets()
     context = {
         'buckets': documents['Buckets'],
-        'location' : session.region_name
+        'location': session.region_name
     }
     return render(request, 'listS3Documents/index.html', context)
+
 
 def bucketContent(request, bucketname, path=None):
     client = boto3.client('s3')
